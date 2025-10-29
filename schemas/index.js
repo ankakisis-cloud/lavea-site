afc0e1501ddaa792d21f332552229fe5eefc8968
ankakisis-cloud <ankakisis@gmail.com>
2025-10-29 23:47:23 +0300

    Create index.js

---
+++ schemas/index.js
@@ -0,0 +1,5 @@
+import project from "./project";
+import review from "./review";
+import price from "./price";
+import contacts from "./contacts";
+export default [project, review, price, contacts];
