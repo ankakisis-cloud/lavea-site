bf37a7d608ee9d3573c2f97b2defc684de4950f0
ankakisis-cloud <ankakisis@gmail.com>
2025-10-29 23:45:52 +0300

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { Studio } from "sanity";
-import config from "../../../sanity.config";
+import config from "../../sanity.config"; // ✅ две точки
 
 export default function AdminPage() {
   return <Studio config={config} />;
