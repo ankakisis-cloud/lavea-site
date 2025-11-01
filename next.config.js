0dfbbe7e728106b303f412b53d04fd87cfd052a1
ankakisis-cloud <ankakisis@gmail.com>
2025-11-01 12:15:18 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+
+  async redirects() {
+    return [
+      // Страницы убрали — перенаправляем:
+      { source: "/reviews", destination: "/", permanent: true },       // отзывы только на главной
+      { source: "/contacts", destination: "/#contacts", permanent: true }, // контакты — якорь на главной
+    ];
+  },
+};
+
+module.exports = nextConfig;
