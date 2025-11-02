54ff6d8076715b104c003f2b935c9b6e9e0ebfc8
ankakisis-cloud <ankakisis@gmail.com>
2025-11-02 18:39:00 +0300

    Update ConsultBtn.js

--- components/ConsultBtn.js
+++ components/ConsultBtn.js
@@ -1,18 +1,13 @@
-// components/ConsultBtn.js
 "use client";
 
 export default function ConsultBtn({ className = "", children = "Получить консультацию" }) {
   const open = () => {
     if (typeof window !== "undefined" && typeof window.openContactModal === "function") {
       window.openContactModal();
-    } else {
-      const el = document.getElementById("contacts");
-      if (el) el.scrollIntoView({ behavior: "smooth" });
     }
   };
-
   return (
-    <button type="button" className={`btn btn-lavea ${className}`} onClick={open}>
+    <button type="button" className={`btn ${className}`} onClick={open}>
       {children}
     </button>
   );
